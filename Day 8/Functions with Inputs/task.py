9f3f8a76000539f2eb53226744de35716f8f34c8
karthik <itsmeshadowmonarch@gmail.com>
2026-03-02 18:49:49 +0530

    Day-8 commit

---
+++ Day 8/Functions with Inputs/task.py
@@ -0,0 +1,12 @@
+# def greet():
+#     print("Hey there!")
+#     print("Welcome!")
+#     print("Enjoy your stay!")
+#
+# greet()
+
+def greet_with_name(name):
+    print(f"Hello, {name}!")
+    print(f"Welcome to Japan, {name}")
+
+greet_with_name("Alex")
---
+++ Day 8/Positional vs Keyword Arguments/task.py
@@ -0,0 +1,18 @@
+# Functions with input
+#
+# def greet_with_name(name):
+#     print(f"Hello {name}")
+#     print(f"How do you do {name}?")
+#
+#
+# greet_with_name("Jack Bauer")
+
+# Functions with more than 1 input
+
+def greet_with(name, location):
+    print(f"Hello, {name}!")
+    print(f"How is it like in {location}?")
+
+greet_with("Alex", "Osaka")
+
+greet_with(location="Nara", name="V")
